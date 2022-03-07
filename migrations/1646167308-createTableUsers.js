b05b66bb29ea7c56362367be7b9320f5b73ee14e
lkk989 <serene.skyline0@gmail.com>
2022-03-07 21:10:57 +0100

    Add database migrations

---
+++ migrations/1646167308-createTableUsers.js
@@ -0,0 +1,16 @@
+exports.up = async (sql) => {
+  await sql`
+		CREATE TABLE users (
+			id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+			name varchar(30) NOT NULL,
+			bio varchar(300) NOT NULL,
+			email varchar(30) NOT NULL UNIQUE,
+			pwhash varchar(90) NOT NULL
+		)`;
+};
+
+exports.down = async (sql) => {
+  await sql`
+		DROP TABLE users
+		`;
+};
