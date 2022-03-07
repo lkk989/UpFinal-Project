a1a04592131e31f6a95aa9401420993962899fbf
lkk989 <serene.skyline0@gmail.com>
2022-03-07 21:14:32 +0100

    Add file for graphQL frontend

---
+++ util/client.js
@@ -0,0 +1,43 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
+
+export const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+  headers: {
+    authorization: 'Bearer users',
+    // add the login token here
+  },
+});
+
+export const usersQuery = gql`
+  query {
+    users {
+      id
+      name
+      bio
+      email
+    }
+  }
+`;
+
+export const createMutation = gql`
+  mutation ($name: String!, $bio: String!, $email: String!, $pw: String!) {
+    createUser(name: $name, bio: $bio, email: $email, pw: $pw) {
+      id
+      name
+      bio
+      email
+    }
+  }
+`;
+
+export const updateMutation = gql`
+  mutation ($id: ID!, $name: String, $bio: String, $email: String) {
+    updateUser(id: $id, name: $name, bio: $bio, email: $email) {
+      id
+      bio
+      name
+      email
+    }
+  }
+`;
