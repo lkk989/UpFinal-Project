30e8ab740317ec9d3644f31e825e701b00497053
lkk989 <serene.skyline0@gmail.com>
2022-03-07 21:18:56 +0100

    Add apollo provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
+import '../styles/global.css';
+import { ApolloProvider } from '@apollo/client';
 import Layout from '../components/Layout';
+import { client } from '../util/client';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ApolloProvider client={client}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ApolloProvider>
   );
 }
 
