8c2433efbb825ffbf6eb22a66b0783999d60bf56
lkk989 <serene.skyline0@gmail.com>
2022-03-07 21:11:52 +0100

    Add type definitions for graphQL

---
+++ pages/api/schema.js
@@ -0,0 +1,22 @@
+import { gql } from '@apollo/client';
+
+// A schema is a collection of type definitions that together define the "shape" of queries
+export const typeDefs = gql`
+  type User {
+    id: ID
+    name: String!
+    bio: String!
+    email: String!
+    pwhash: String!
+  }
+
+  type Query {
+    users: [User]
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    createUser(name: String!, bio: String!, email: String!, pw: String!): User
+    updateUser(id: ID!, name: String, bio: String, email: String): User
+  }
+`;
