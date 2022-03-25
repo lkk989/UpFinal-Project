d0fd604befc8a2be93985965864c0ad3e7af455c
lkk989 <serene.skyline0@gmail.com>
2022-03-25 15:57:26 +0100

    Add styles and dropdown menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,79 @@
 import { css, Interpolation, Theme } from '@emotion/react';
+import Image from 'next/image';
 import Link from 'next/link';
-import { AnchorHTMLAttributes } from 'react';
+import { AnchorHTMLAttributes, useState } from 'react';
 
 const headerStyles = css`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  p {
+  .nav {
+    position: fixed;
+    z-index: 2;
+    top: 0;
+    left: 0;
+    right: 0;
     margin: 0;
+    background-color: #fffffff7;
+    border-bottom: 3px solid;
+    border-image-slice: 1;
+    border-width: 4px;
+    border-image-source: linear-gradient(
+      to right,
+      #05396b,
+      #389583,
+      #8de4af,
+      #bff0d1
+    );
+    display: flex;
+    justify-content: space-between;
+    height: 50px;
+  }
+  button {
+    background-color: transparent;
+    border: none;
+    margin: 10px 6px;
+  }
+  .closed {
+    display: none;
+  }
+  .open {
+    display: flex;
+    padding: 10px;
+    border-right: 3px solid #389583;
+    border-bottom: 3px solid #389583;
+    border-bottom-right-radius: 4px;
+    position: fixed;
+    z-index: 2;
+    top: 50px;
+    left: 0;
+    right: 70%;
+    margin: 0;
+    background-color: white;
+    flex-direction: column;
+    a,
+    Anchor,
+    p {
+      margin: 0;
+      color: #545659;
+      text-decoration: none;
+    }
+  }
+  /* .user {
+    a {
+      color: white;
+    }
+  } */
+  .avatar {
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    margin: 0 10px;
+    border-radius: 50%;
+    outline: 2px solid #545659;
+    overflow: hidden;
+    transform: translateY(10px);
+    img {
+      height: 100%;
+      width: 100%;
+    }
   }
 `;
 
@@ -15,6 +81,7 @@ type Props = {
   user: {
     id: number;
     name: string;
+    avatar: string;
   };
 };
 
@@ -28,17 +95,52 @@ function Anchor({
 }
 
 export default function Header(props: Props) {
+  const [menu, setMenu] = useState('closed');
+
   return (
     <div css={headerStyles}>
-      <p>
-        Logged in as{' '}
-        <Link href={`/users/${props.user.id}`}>
-          <a>{props.user.name}</a>
-        </Link>
-      </p>
-      <p>
-        <Anchor href="/logout">Logout</Anchor>
-      </p>
+      <div className="nav">
+        <button onClick={() => setMenu(menu === 'closed' ? 'open' : 'closed')}>
+          <Image
+            src={menu === 'closed' ? '/menuIcon.png' : '/closeMenuIcon.png'}
+            width="25px"
+            height="25px"
+          />
+        </button>
+        <div className={`menu ${menu}`}>
+          <Link href={`/users/${props.user.id}`}>
+            <a>Dashboard</a>
+          </Link>
+          <Link href="/matches">
+            <a>Matches</a>
+          </Link>
+          <Link href="/profile">
+            <a>
+              <Image src="/editIcon.png" width="17px" height="17px" alt="" />{' '}
+              Profile
+            </a>
+          </Link>
+          <p>
+            <Anchor href="/logout">
+              <Image src="/logoutIcon.png" width="17px" height="17px" alt="" />{' '}
+              Logout
+            </Anchor>
+          </p>
+        </div>
+        <div className="user">
+          <Link href={`/users/${props.user.id}`}>
+            <a>
+              {props.user.name}
+              <div className="avatar">
+                <img
+                  src={props.user.avatar}
+                  alt={`user avatar of a ${props.user.avatar.slice(1, -4)}`}
+                />
+              </div>
+            </a>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
