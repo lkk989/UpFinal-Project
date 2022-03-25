1f0f97b96a56fd5bf786abc24b9caba7ad575311
lkk989 <serene.skyline0@gmail.com>
2022-03-25 16:03:15 +0100

    Add types, avatar, chat and message mutations

--- pages/api/schema.js
+++ pages/api/schema.js
@@ -8,6 +8,7 @@ export const typeDefs = gql`
     bio: String
     email: String
     pwhash: String
+    avatar: String
   }
   type Id {
     id: ID
@@ -19,25 +20,43 @@ export const typeDefs = gql`
     userId: ID
     activityId: ID
   }
+  type ChatUser {
+    userId: ID
+    chatId: ID
+  }
+  type Chat {
+    id: ID
+    name: String
+  }
+  type Message {
+    id: ID
+    userId: ID
+    chatId: ID
+    content: String
+    timestamp: String
+    name: String
+  }
   type Error {
     error: String
   }
 
   type Query {
     users: [User]
-    user(id: ID!): User
+    user: User
+    messageHistory(chatId: ID!): [Message]
   }
 
   type Mutation {
     createUser(
       name: String!
+      avatar: String
       bio: String!
       email: String!
       pw: String!
       csrfToken: String!
     ): User
 
-    updateUser(id: ID!, name: String!, bio: String!): User
+    updateUser(id: ID!, name: String!, avatar: String, bio: String!): User
 
     logUserIn(email: String!, pw: String!, csrfToken: String!): Id
 
@@ -46,5 +65,13 @@ export const typeDefs = gql`
     deleteAllUserActivities(userId: ID!): Id
 
     deleteUser(id: ID!): Name
+
+    createNewChat(name: String!): Chat
+
+    addChatUser(userId: ID!, chatId: ID!): ChatUser
+
+    deleteChatUser(userId: ID!, chatId: ID!): ChatUser
+
+    storeMessage(chatId: ID!, content: String!, name: String!): Message
   }
 `;
