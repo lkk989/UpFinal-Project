993ea2eabfea9765b5b921f0804870a318dc4112
lkk989 <serene.skyline0@gmail.com>
2022-03-25 16:06:06 +0100

    Add background img, adjust css

--- pages/_app.js
+++ pages/_app.js
@@ -14,3 +14,18 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+// baby duck photo
+// Photo by <a href="https://unsplash.com/@tunebasics?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Kerin Gedge</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+// puppy
+// Photo by <a href="https://unsplash.com/@blueskiesburning?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ignacio R</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+// kitten
+// Photo by <a href="https://unsplash.com/@elodieoudot?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Elodie Oudot</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+// picknick in a park photo
+// Photo by <a href="https://unsplash.com/@masondahl?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Mason Dahl</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+// paper airplane
+// <a href="https://www.flaticon.com/free-icons/mail" title="mail icons">Mail icons created by Freepik - Flaticon</a>
