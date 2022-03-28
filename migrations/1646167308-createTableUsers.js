3682e864eb01da1ffa4bd636ef9dc0e89b2bd55f
lkk989 <serene.skyline0@gmail.com>
2022-03-29 00:15:31 +0200

    Add references, add chat admin

--- migrations/1646167308-createTableUsers.js
+++ migrations/1646167308-createTableUsers.js
@@ -3,7 +3,7 @@ exports.up = async (sql) => {
 		CREATE TABLE users (
 			id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 			name varchar(30) NOT NULL,
-			avatar varchar(150) NOT NULL,
+			avatar varchar(80) NOT NULL,
 			bio varchar(300) NOT NULL,
 			email varchar(30) NOT NULL UNIQUE,
 			pwhash varchar(90) NOT NULL
--- migrations/1647607189-createTableChats.js
+++ migrations/1647607189-createTableChats.js
@@ -2,7 +2,8 @@ exports.up = async (sql) => {
   await sql`
 		CREATE TABLE chats (
 			id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-			name varchar(50) NOT NULL
+			name varchar(50) NOT NULL,
+			user_id integer REFERENCES users (id)
 		)`;
 };
 
