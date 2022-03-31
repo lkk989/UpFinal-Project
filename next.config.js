56b7cb1c96b3f2afc43e661ecef3dae835087319
lkk989 <serene.skyline0@gmail.com>
2022-03-31 14:56:59 +0200

    Configure PWA

--- next.config.js
+++ next.config.js
@@ -10,7 +10,9 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   pwa: {
     dest: 'public',
-    register: true,
-    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+    cacheOnFrontEndNav: true,
+    // register: true,
+    // skipWaiting: true,
   },
 });
