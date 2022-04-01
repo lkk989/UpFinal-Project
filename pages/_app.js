5aa122ba177841d2251a8ef14f54e62c790060c0
lkk989 <serene.skyline0@gmail.com>
2022-04-01 11:02:30 +0200

    Delete _document, save in _app as comment

--- pages/_app.js
+++ pages/_app.js
@@ -35,3 +35,42 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
+// import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+// class MyDocument extends Document {
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <meta
+//             name="description"
+//             content="Buddies - the chat app to find your people"
+//           />
+//           <meta name="mobile-web-app-capable" content="yes" />
+//           <link rel="manifest" href="/manifest.json" />
+//           <link rel="apple-touch-icon" href="/icon-apple-touch.png" />
+//           <link
+//             rel="icon"
+//             type="image/png"
+//             sizes="192x192"
+//             href="/icon-192.png"
+//           />
+//           <link
+//             rel="icon"
+//             type="image/png"
+//             sizes="512x512"
+//             href="/icon-512.png"
+//           />
+//           <meta name="theme-color" content="#05396b" />
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
+
+// export default MyDocument;
