8d107adec0bb1f8d6af1ec6cf1acfc473e80cd93
lkk989 <serene.skyline0@gmail.com>
2022-04-13 15:57:23 +0200

    Add foreign key restraint to user and chat ids

--- migrations/1647609223-createTableMessages.js
+++ migrations/1647609223-createTableMessages.js
@@ -2,8 +2,8 @@ exports.up = async (sql) => {
   await sql`
 		CREATE TABLE messages (
 			id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-			user_id integer,
-			chat_id integer,
+			user_id integer REFERENCES users(id),
+			chat_id integer REFERENCES chats(id),
 			timestamp timestamp NOT NULL DEFAULT NOW(),
 			content varchar(600) NOT NULL,
 			name varchar(30) NOT NULL
